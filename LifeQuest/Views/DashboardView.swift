29193a657dbb7838d6fc52021051dd0b4eaee007
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-08 09:50:25 -0400

    Fixed name bug

--- LifeQuest/Views/DashboardView.swift
+++ LifeQuest/Views/DashboardView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct CharacterViewModel: View {
+struct DashboardView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CharacterViewModel()
+    DashboardView()
 }
