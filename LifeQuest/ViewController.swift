71dad34be4b39346e9a093a5efdb76793c6c6a17
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-08 09:06:05 -0400

    Initial Commit

---
+++ LifeQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeQuest
+//
+//  Created by Icy on 9/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LifeQuestTests/LifeQuestTests.swift
@@ -0,0 +1,19 @@
+//
+//  LifeQuestTests.swift
+//  LifeQuestTests
+//
+//  Created by Icy on 9/8/26.
+//
+
+import Testing
+@testable import LifeQuest
+
+struct LifeQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
