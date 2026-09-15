ff16ab1b0f606f4409a649509c391f968897619a
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-15 09:34:09 -0400

    RED: Stats cannot exceed cap of 99

---
+++ LifeQuestTests/ViewModels/CharacterViewModelsTests.swift
@@ -0,0 +1,32 @@
+//
+//  CharacterViewModelsTests.swift
+//  LifeQuestTests
+//
+//  Created by Icy on 9/15/26.
+//
+
+import Testing
+@testable import LifeQuest
+
+struct CharacterViewModelsTests {
+
+    @Test("Stats cannot exceed cap of 99", arguments:
+    [
+        (50,10,60),
+        (89,10,99),
+        (95,15,99),
+        (99,100,99)
+    ])
+    func testAddStat_RespectsBoundryCap( startStat: Int, amountAdded: Int, expectedResult: Int){
+        //Arrange
+        let sut = CharacterViewModel()
+        sut.character.strength = startStat
+        
+        //Act
+        sut.addStat(category: .strength, amount: amountAdded)
+        
+        //Assert
+        #expect(sut.character.strength == expectedResult)
+    }
+
+}
