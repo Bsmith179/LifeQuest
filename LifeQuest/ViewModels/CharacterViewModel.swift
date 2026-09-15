41d442da003b66f3d0126b4048197fff08ebb1b2
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-15 10:03:15 -0400

    GREEN: Stats cannot exceed cap of 99

--- LifeQuest/ViewModels/CharacterViewModel.swift
+++ LifeQuest/ViewModels/CharacterViewModel.swift
@@ -13,8 +13,37 @@ class CharacterViewModel: ObservableObject {
     @Published var character = Character()
     let xpThreshold: Int = 1000
     
-    func gainXp(amount: Int) {}
-    func checkLevelUp() {}
-    func addStat(category: StatCategory,  amount: Int) {}
-    func deductStat(category: StatCategory,  amount: Int) {}
+    
+
+    func gainXp(amount: Int) {
+        
+    }
+    
+    func checkLevelUp() {
+        
+    }
+    
+    func addStat(category: StatCategory,  amount: Int) {
+        let maxCap: Int = 99
+        
+        switch category {
+        case .strength:
+            character.strength = min(character.strength + amount, maxCap)
+            break
+            
+        case .intelligence:
+            character.intelligence += amount
+            break
+        case .charisma:
+            character.charisma += amount
+            break
+        case .focus:
+            character.focus += amount
+            break
+        }
+    }
+    
+    func deductStat(category: StatCategory,  amount: Int) {
+        
+    }
 }
