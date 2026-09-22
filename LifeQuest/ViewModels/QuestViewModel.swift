4c415f347337b6a5345177c5d19d3042efc9a041
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-22 11:29:17 -0400

    GREEN: Cannot create a duplicate quest

--- LifeQuest/ViewModels/QuestViewModel.swift
+++ LifeQuest/ViewModels/QuestViewModel.swift
@@ -13,8 +13,12 @@ class QuestViewModel: ObservableObject {
     @Published var activeQuests: [Quest] = []
     
     func createNewQuest(title: String,  categories: [StatCategory],  difficulty: Double,  frequency: QuestFrequency) {
-        let quest = Quest(title: title, categories: categories, difficultyMultiplier: difficulty, frequency: frequency)
-        activeQuests.append(quest)
+        if !activeQuests.contains(where: { $0.title == title}){
+            
+            let quest = Quest(title: title, categories: categories, difficultyMultiplier: difficulty, frequency: frequency)
+            activeQuests.append(quest)
+        }
+
     }
  
     func completeQuest(questID: UUID) ->Quest? {
