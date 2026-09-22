dd51ef8fb23b4f73157b8f9e895d4bb9c086b06e
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-22 11:26:54 -0400

    RED: Cannot create a duplicate quest

--- LifeQuestTests/ViewModels/QuestViewModelTest.swift
+++ LifeQuestTests/ViewModels/QuestViewModelTest.swift
@@ -22,4 +22,15 @@ struct QuestViewModelTest {
         #expect(sut.activeQuests.count == 1)
     }
 
+    @Test("Cannot create a duplicate quest")
+    
+    func testCreateNewQuest_PreventDuplicates(){
+        let sut = QuestViewModel()
+        
+        sut.createNewQuest(title: "Test Quest", categories: [StatCategory.strength], difficulty: 2.0, frequency: QuestFrequency.daily)
+        
+        sut.createNewQuest(title: "Test Quest", categories: [StatCategory.strength], difficulty: 2.0, frequency: QuestFrequency.daily)
+        
+        #expect(sut.activeQuests.count == 1)
+    }
 }
