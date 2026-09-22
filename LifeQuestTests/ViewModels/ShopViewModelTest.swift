65d17252868b8bf1e78f06bc2b9d2a51d7f574f0
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-09-22 09:55:54 -0400

    RED: No Duplicate rewards can be added

--- LifeQuestTests/ViewModels/ShopViewModelTest.swift
+++ LifeQuestTests/ViewModels/ShopViewModelTest.swift
@@ -20,5 +20,16 @@ struct ShopViewModelTest {
         #expect(sut.availableRewards.count == 1)
         
     }
-
+    
+    @Test("No Duplicate rewards can be added")
+    func testCreateReward_PreventDuplicates(){
+        
+        let sut = ShopViewModel()
+        
+        sut.createReward(name: "Test Reward", goldCost: 10)
+        sut.createReward(name: "Test Reward", goldCost: 10)
+        
+        #expect(sut.availableRewards.count == 1)
+        
+    }
 }
